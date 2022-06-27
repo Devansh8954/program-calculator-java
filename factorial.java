190b39f0967d4b8c2a1a6c73db0f3416f69caa5b
Devansh <devansh8954@gmail.com>
2022-06-27 15:25:03 +0530

    updateloop

--- factorial.java
+++ factorial.java
@@ -7,9 +7,16 @@ class factorial{
      int i,fact=1;  
      System.out.println("Enter a number in which you have to find Factorial");
      int num=sc.nextInt();//It is the number to calculate factorial    
-     for(i=1;i<=num;i++){    
-         fact=fact*i;    
-     }    
-     System.out.println("Factorial of "+num+" is: "+fact);    
+     if((num==0)||(num<0))
+     {
+        System.out.println("Factorial is 1");    
+     }
+     else{
+     for(i=1;i<=num;i++)
+     {    
+         fact=fact*i;         
+     }
+     System.out.println("Factorial of "+num+" is: "+fact);
+    }
     }  
    }  
