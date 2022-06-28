456b82dd297377e027625b6bac39a8af9fc323d1
Devansh <devansh8954@gmail.com>
2022-06-28 14:55:24 +0530

    updatecondition

--- table.java
+++ table.java
@@ -6,10 +6,16 @@ public class table
         //It is the number to calculate
         System.out.println("Enter a number to find Table");
         int num = sc.nextInt();
+        if(num<=0)
+        {
+            System.out.println("Error! Please Enter Correct number");
+        }
+        else {
         for(int i=1; i<=10; i++)
         {
             int tab = num*i;
             System.out.println(num+"*"+i+"="+tab);
         }
+     }
     }
 }
